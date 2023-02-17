2fd21062707a0fc182ae24339f5f5526fb62eb27
Abdelrahman <ch4e_90@hotmail.com>
2023-02-17 15:02:49 +0200

    update vm

--- terraform/3-vm.tf
+++ terraform/3-vm.tf
@@ -22,5 +22,17 @@ resource "google_compute_instance" "private-instance" {
     subnetwork = google_compute_subnetwork.management_subnet.id
 
   }
-
+  #Install kubectl, dockercli , google cloud auth plugin
+  metadata = {
+    startup-script = <<-EOF
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    apt-get update -y && apt-get install ca-certificates curl gnupg lsb-release -y && mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+    EOF
+  }
 }
